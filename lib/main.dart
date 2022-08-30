7ffe31dd0615b9e769f6c9f735714d7ce79cba8f
HashirHamid <Hashir.f01@gmail.com>
2022-08-30 14:19:28 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'diagnosis_screen.dart';
+import 'home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontFamily: 'Raleway',
+              ),
+              headline5: TextStyle(
+                color: Color.fromARGB(255, 239, 236, 236),
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => HomeScreen(),
+      },
+    );
+  }
+}
